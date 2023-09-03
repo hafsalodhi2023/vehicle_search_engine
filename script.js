46ec41d72c57dab063d756abe7ebb1f430b8b076
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2023-09-03 20:24:10 +0500

    Added Preloader and Default Buttons

--- script.js
+++ script.js
@@ -2,22 +2,33 @@ const userInput = document.querySelector("#userInput");
 const searchType = document.querySelector("#searchType");
 const searchBtn = document.querySelector("#searchBtn");
 const resultTableBody = document.querySelector("#resultTableBody");
+const preLoader = document.querySelector("#pre-loader");
+const ResetBtn = document.querySelector("#reset-btn");
+
+function preLoaderDisplayNone() {
+  preLoader.style.display = "none";
+}
+function preLoaderDisplayBlock() {
+  preLoader.style.display = "block";
+}
 
 async function checkData() {
   let url =
-    "https://script.google.com/macros/s/AKfycbxyEBkGmIcxdiod3GDPV5JTTGqq_0Dik5MoKHBtIJxGRBAtACMBjIeX5ONgsz2U2ZTsrg/exec";
+    "https://script.google.com/macros/s/AKfycbzXylmVnrzyKIPES9-ZmdXQCKreCi9BitKbCJ1jqN6JtTFzYyVBltJSu8OowXENT7allw/exec";
 
   try {
     const response = await fetch(
       `${url}?${searchType.value}=${userInput.value}`
     );
     if (userInput.value == "") {
+      preLoaderDisplayNone();
       alert("Please Type A Number.");
       return;
     }
     const actualData = await response.json();
 
     if (actualData.data.length === 0) {
+      preLoaderDisplayNone();
       alert("No data found.");
       resultTableBody.innerHTML = "";
     } else {
@@ -44,14 +55,33 @@ function displayDataInTable(data) {
       <td>${item.chassisNo}</td>
     `;
 
+    ResetBtn.addEventListener("click", () => {
+      userInput.value = "";
+      resultTableBody.innerHTML = "";
+    });
+
     resultTableBody.appendChild(row);
+    if (resultTableBody.innerHTML != "") {
+      preLoaderDisplayNone();
+    } else {
+      preLoaderDisplayBlock();
+    }
   });
 }
 
 window.onload = () => {
   userInput.value = "";
+
+  userInput.onkeydown = () => {
+    if (event.key === "Enter") {
+      checkData();
+      preLoaderDisplayBlock();
+      resultTableBody.innerHTML = "";
+    }
+  };
 };
 
 searchBtn.addEventListener("click", function () {
   checkData();
+  preLoaderDisplayBlock();
 });
